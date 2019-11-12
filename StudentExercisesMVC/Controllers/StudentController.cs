7aa8e908fd4e0c69d3f868af907dcbd0f6ccb47f
Andy Collins <andy.collins@relode.com>
2019-11-12 07:47:52 -0600

    Add project files.

---
+++ StudentExercisesMVC/Controllers/StudentController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesMVC.Models;
+
+namespace StudentExercisesMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private string _connectionString;
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        public StudentController(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT s.id, s.firstname, s.lastname, 
+                                               s.slackhandle, s.cohortId,
+                                               c.name as cohortname
+                                         FROM Student s INNER JOIN Cohort c on c.id = s.cohortid";
+                    var reader = cmd.ExecuteReader();
+                    var students = new List<Student>();
+                    while (reader.Read())
+                    {
+                        students.Add(
+                            new Student
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                                FirstName = reader.GetString(reader.GetOrdinal("firstname")),
+                                LastName = reader.GetString(reader.GetOrdinal("lastname")),
+                                SlackHandle = reader.GetString(reader.GetOrdinal("slackhandle")),
+                                CohortId = reader.GetInt32(reader.GetOrdinal("cohortId")),
+                                Cohort = new Cohort()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("cohortId")),
+                                    Name = reader.GetString(reader.GetOrdinal("cohortname")),
+                                }
+                            });
+                    }
+
+                    reader.Close();
+                    return View(students);
+                }
+            }
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            var student = GetById(id);
+            return View(student);
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var student = GetById(id);
+            return View(student);
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Student WHERE id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private Student GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT s.id, s.firstname, s.lastname, 
+                                               s.slackhandle, s.cohortId,
+                                               c.name as cohortname
+                                         FROM Student s INNER JOIN Cohort c on c.id = s.cohortid
+                                        WHERE s.id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    var reader = cmd.ExecuteReader();
+
+                    Student student = null;
+                    if (reader.Read())
+                    {
+                        student = new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("firstname")),
+                            LastName = reader.GetString(reader.GetOrdinal("lastname")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("slackhandle")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("cohortId")),
+                            Cohort = new Cohort()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("cohortId")),
+                                Name = reader.GetString(reader.GetOrdinal("cohortname")),
+                            }
+                        };
+                    }
+                    reader.Close();
+                    return student;
+                }
+            }
+        }
+    }
+}
---
+++ StudentExercisesMVC/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Surname")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Slack")]
+        public string SlackHandle { get; set; }
+
+        [Display(Name = "Cohort")]
+        public int CohortId { get; set; }
+
+        public Cohort Cohort { get; set; }
+    }
+}
